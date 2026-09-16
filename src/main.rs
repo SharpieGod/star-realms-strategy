2d1c0a92917267a059bd5c808739a01e4145b408
darkos-ubuntu-machine <sabodarko09@gmail.com>
2026-09-15 19:05:07 -0700

    better rate tracking lol

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ mod game;
 mod player;
 mod util;
 
-use std::{io::stdin, time::Instant};
+use std::{
+    io::stdin,
+    time::{Duration, Instant},
+};
 
 use agents::{BasicBot, PassBot10000, UserCLI};
 use game::Game;
@@ -23,12 +26,27 @@ fn main() {
 
     // game.run_game(&mut [Box::new(cli), Box::new(basic)]);
 
-    let start = Instant::now();
+    let mut last_check = Instant::now();
 
     let mut player_wins = [0, 0];
+    const CHECK_INTERVAL: usize = 100;
+    let mut count = 0;
 
-    for i in 0..100000 {
-        println!("{:.2} games/s", i as f32 / start.elapsed().as_secs_f32());
+    for i in 0..200000 {
+        if i % CHECK_INTERVAL == 0 {
+            let elapsed = last_check.elapsed();
+            if elapsed >= Duration::from_millis(500) {
+                let rate = count as f64 / elapsed.as_secs_f64();
+                println!("Estimated Rate: {:.2} iter/sec", rate);
+
+                // Reset for the next window
+                last_check = Instant::now();
+                count = 0;
+            }
+        }
+
+        count += 1;
+
         let mut game = Game::new();
         let agent1 = BasicBot {};
         let agent2 = BasicBot {};
